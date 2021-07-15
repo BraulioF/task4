7c9d49c139629bf0e4a1227bf9150a6b62776620
BraulioF <bfernandez@adnetworks.cl>
2021-07-15 12:26:56 -0400

    subiendo categoria

---
+++ models/product_categoria/resources.py
@@ -0,0 +1,15 @@
+from ..auth import odoo 
+
+
+class ProductCategList ():
+    """get list of product categ"""
+    def getCateg(data):
+        """Get categ_name by categ name"""
+        odoo_client = odoo.OdooClient()
+        uid, models = odoo_client.logging()
+        product = data["create_prod"]
+        parners_details = models.execute_kw(odoo_client.db, uid, odoo_client.password,
+                        'product.category', 'search_read',
+                [[['name', '=', product["categ_name"]]]],
+                { 'fields': ['id'] ,'limit': 1})
+        return parners_details 
